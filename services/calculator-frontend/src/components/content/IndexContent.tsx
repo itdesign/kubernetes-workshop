d8fcaa4b4fefbab69ed030653fe6c97217ff309c
Jakob Jarosch <dev@jakobjarosch.de>
2019-04-25 23:44:26 +0200

    Show history link also on index

--- services/calculator-frontend/src/components/content/IndexContent.tsx
+++ services/calculator-frontend/src/components/content/IndexContent.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { BaseLayout } from '../BaseLayout';
 import { reactRender } from '../../utils/reactRender';
 import { SearchForm } from '../SearchForm';
+import { isHistoryEnabled } from '../../utils/appVersion';
 
 const IndexContent: React.FunctionComponent<{}> = () => (
   <BaseLayout>
     <SearchForm />
+    <p>{isHistoryEnabled() && <a href="history">Show History</a>}</p>
   </BaseLayout>
 );
 
